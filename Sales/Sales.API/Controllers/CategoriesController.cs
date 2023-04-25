c0280ff7a76ad5c22421ce089d06acb479dcb777
egil <egil@mobiletec.net>
2023-04-25 18:16:58 -0500

    Add CategoriesController

---
+++ Sales/Sales.API/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {        
+        private readonly DataContext _context;
+
+        public CategoriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Categories.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return Ok(category);
+        }
+    }
+}
